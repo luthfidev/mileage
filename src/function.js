5c5e0958b17424ba26ece15073f711c2bc8c0675
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-06 15:21:59 +0700

    Update function.js

--- src/function.js
+++ src/function.js
@@ -1,13 +1,20 @@
-function jaraktempuh(){
+function jaraktempuh(waktuawal,waktuakhir){
 
     // s = v x t
 
     var waktuawal = '10:12:21'
     var waktuakhir = '12:00:00'
 
+    var selisih = parseInt(waktuakhir) - parseInt(waktuawal);
+    if (!isNaN(selisih)) {
+       console.log(selisih)
+    }
+
+
+
     var s = 8 * 120
     
-    console.log(s)
+    console.log(s) 
 
 
 
