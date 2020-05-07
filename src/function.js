58a4973b18574999c8eed6f9bd8d956506cc2e2f
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-07 08:06:20 +0700

    Update function.js

--- src/function.js
+++ src/function.js
@@ -1,14 +1,15 @@
-const jaraktempuh = () => {
+const jaraktempuh = (start, end) => {
 
     let speed       = 6
     let jaraktempuh = 0
     let second      = 0
+   // let awalWaktu        = '10:12:21'
 
-   awal = new Date('2020-01-01T10:12:21').getTime()
-   akhir = new Date('2020-01-01T12:00:00').getTime()
+   var awal = new Date('2020-01-01T'+start).getTime()
+   var akhir = new Date('2020-01-01T'+end).getTime()
   
+   /* console.log(awal) */
    let diff = (akhir-awal) / 1000
-   
 
 
    while(diff !== 0 ){
@@ -16,7 +17,7 @@ const jaraktempuh = () => {
        second++ //nilai second ini akan menjadi 1
 
        let timeInminutes = second / 60 //untuk mencari menit maka 1 / 60
-       
+
        if(timeInminutes === 5){
            speed+=2
           
@@ -28,7 +29,7 @@ const jaraktempuh = () => {
            }   
        }
    jaraktempuh+=speed
-   console.log(jaraktempuh)
+   
  }
    
 return jaraktempuh
